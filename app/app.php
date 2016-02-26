76f823272c334fc64328124fc7ac3a6b97e00be2
Aundra Miller <miller.aundra@gmail.com>
2016-02-26 13:45:00 -0800

    add routes and pages to provide functionality for editing and deleting individual stylists

--- app/app.php
+++ app/app.php
@@ -38,6 +38,42 @@
         return $app['twig']->render('stylist.html.twig', array('stylist'=>$stylist, 'clients'=>$matching_clients));
     });
 
+/* Individual Stylist pages */
+    $app->get('/client/add/{id}', function($id) use ($app) {
+        $stylist = Stylist::find($id);
+        return $app['twig']->render('new_client.html.twig', array('stylist' => $stylist));
+    });
+
+/* Route to Edit or Delete Stylist */
+    $app->get('/stylist/update/{id}', function($id) use ($app) {
+        $stylist = Stylist::find($id);
+        return $app['twig']->render('stylist_edit_delete.html.twig', array('stylist' => $stylist));
+    });
+
+    $app->post('/stylist/update/{id}', function($id) use ($app) {
+        $stylist = Stylist::find($id);
+        $stylist->update($_POST['name'], $_POST['services'], $_POST['phone']);
+        $matching_clients = $stylist->getClients();
+        return $app['twig']->render('stylist.html.twig', array('stylist'=>$stylist, 'clients'=>$matching_clients));
+    });
+
+    $app->post('/stylist/delete/{id}', function($id) use ($app) {
+        $stylist = Stylist::find($id);
+        $stylist->delete();
+        $stylists = Stylist::getAll();
+        return $app['twig']->render('index.html.twig', array('stylists'=>$stylists));
+    });
+
+/* Add client to single stylist */
+    $app->post('/client/add/{id}/submit', function($id) use ($app) {
+        $new_client = new Client($_POST['name'], $_POST['number'], $id);
+        $new_client->save();
+        $stylist = Stylist::find($id);
+        $matching_clients = $stylist->getClients();
+        return $app['twig']->render('stylist.html.twig', array('stylist'=>$stylist, 'clients'=>$matching_clients));
+    });
+
     return $app;
+
 
  ?>
