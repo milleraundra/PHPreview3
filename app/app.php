570a819f9c5d086da06af38ad860f2edc3af8d31
Aundra Miller <miller.aundra@gmail.com>
2016-02-26 10:43:17 -0800

    create index page and route

---
+++ app/app.php
@@ -0,0 +1,28 @@
+<?php
+
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Stylist.php";
+
+    $app = new Silex\Application();
+
+    $server = 'mysql:host=localhost;dbname=hair_salon';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    // use Symfony\Component\HttpFoundation\Request;
+    // Request::enableHttpMethodParameterOverride();
+
+    /* Home page */
+    $app->get('/', function() use ($app) {
+        $stylists = Stylist::getAll();
+        return $app['twig']->render('index.html.twig', array('stylists'=>$stylists));
+    });
+
+    return $app;
+
+ ?>
