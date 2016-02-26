f86ac87bf65674f989b59ff53b1c65bb5c5145bd
Aundra Miller <miller.aundra@gmail.com>
2016-02-26 09:19:55 -0800

    create getters, setters, save, getall, deleteall, and find methods for stylist class

---
+++ src/Stylist.php
@@ -0,0 +1,105 @@
+<?php
+
+    class Stylist
+    {
+        private $name;
+        private $services;
+        private $phone;
+        private $id;
+
+        function __construct($name, $services, $phone, $id = null)
+        {
+            $this->name = $name;
+            $this->services = $services;
+            $this->phone = $phone;
+            $this->id = $id;
+        }
+
+        function setName($new_name)
+        {
+            $this->name = $new_name;
+        }
+
+        function getName()
+        {
+            return $this->name;
+        }
+
+        function setServices($new_services)
+        {
+            $this->services = $new_services;
+        }
+
+        function getServices()
+        {
+            return $this->services;
+        }
+
+        function setPhone($new_phone)
+        {
+            $this->phone = $new_phone;
+        }
+
+        function getPhone()
+        {
+            return $this->phone;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function adjustPunctuation($input)
+        {
+            $search = "/(\')/";
+            $replace = "\'";
+            $clean_input = preg_replace($search, $replace, $input);
+            return $clean_input;
+        }
+
+        function save()
+        {
+            $this->setServices($this->adjustPunctuation($this->getServices()));            $GLOBALS['DB']->exec("INSERT INTO stylists (name, services, phone) VALUES ('{$this->getName()}', '{$this->getServices()}', '{$this->getPhone()}');");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        static function getAll()
+        {
+            $returned_stylists = $GLOBALS['DB']->query("SELECT * FROM stylists");
+            $all_stylists = array();
+            foreach ($returned_stylists as $stylist){
+                $name = $stylist['name'];
+                $services = $stylist['services'];
+                $phone = $stylist['phone'];
+                $id = $stylist['id'];
+                $new_stylist = new Stylist($name, $services, $phone, $id);
+                array_push($all_stylists, $new_stylist);
+            }
+            return $all_stylists;
+        }
+
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM stylists;");
+        }
+
+        static function find($id)
+        {
+            $all_stylists = Stylist::getAll();
+            $found_stylist = null;
+            foreach($all_stylists as $stylist){
+                $stylist_id = $stylist->getId();
+                if ($stylist_id == $id) {
+                    $found_stylist = $stylist;
+                }
+            }
+            return $found_stylist;
+        }
+
+    }
+
+
+
+
+ ?>
--- tests/StylistTest.php
+++ tests/StylistTest.php
@@ -1,6 +1,168 @@
 <?php
 
+/**
+* @backupGlobals disabled
+* @backupStaticAttributes disabled
+*/
 
+    require_once __DIR__."/../src/Stylist.php";
+
+    $server = 'mysql:host=localhost;dbname=hair_salon_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class StylistTest extends PHPUnit_Framework_TestCase
+    {
+
+        protected function tearDown()
+        {
+            Stylist::deleteAll();
+        }
+
+        function test_allGetters()
+        {
+            $name = "Gretchen";
+            $services = "Cut/Style, Color, Highlights";
+            $phone = "5033902341";
+            $id = null;
+
+            $stylist_test = new Stylist($name, $services, $phone, $id);
+
+            $result1 = $stylist_test->getName();
+            $result2 = $stylist_test->getServices();
+            $result3 = $stylist_test->getPhone();
+
+            $this->assertEquals($name, $result1);
+            $this->assertEquals($services, $result2);
+            $this->assertEquals($phone, $result3);
+
+        }
+
+        function test_getId()
+        {
+            $name = "Gretchen";
+            $services = "Cut/Style, Color, Highlights";
+            $phone = "5033902341";
+            $id = 1;
+
+            $stylist_test = new Stylist($name, $services, $phone, $id);
+
+            $result = $stylist_test->getId();
+
+            $this->assertEquals($id, is_numeric($result));
+        }
+
+        function test_adjustPunctuation()
+        {
+            $name = "Marika";
+            $services = "Cut/Style, Color, Men's Cut";
+            $phone = "5032212348";
+            $id = null;
+            $test_stylist = new Stylist($name, $services, $phone, $id);
+
+            $result = $test_stylist->adjustPunctuation($services);
+
+            $this->assertEquals("Cut/Style, Color, Men\'s Cut", $result);
+        }
+
+        function test_save()
+        {
+            $name = "Gretchen";
+            $services = "Cut/Style, Color, Highlights";
+            $phone = "5033902341";
+            $id = null;
+            $stylist_test = new Stylist($name, $services, $phone, $id);
+            $stylist_test->save();
+
+            $result = Stylist::getAll();
+
+            $this->assertEquals($stylist_test, $result[0]);
+        }
+
+        function test_getAll()
+        {
+            $name = "Gretchen";
+            $services = "Cut/Style, Color, Highlights";
+            $phone = "5033902341";
+            $id = null;
+            $stylist_test = new Stylist($name, $services, $phone, $id);
+            $stylist_test->save();
+
+            $name2 = "Mike";
+            $services2 = "Cut/Style, Mens Cut";
+            $phone2 = "971234903";
+            $stylist_test2 = new Stylist($name2, $services2, $phone2, $id);
+            $stylist_test2->save();
+
+            $result = Stylist::getAll();
+
+            $this->assertEquals([$stylist_test, $stylist_test2], $result);
+        }
+
+        function test_deleteAll()
+        {
+            $name = "Gretchen";
+            $services = "Cut/Style, Color, Highlights";
+            $phone = "5033902341";
+            $id = null;
+            $stylist_test = new Stylist($name, $services, $phone, $id);
+            $stylist_test->save();
+
+            $name2 = "Mike";
+            $services2 = "Cut/Style, Mens Cut";
+            $phone2 = "971234903";
+            $stylist_test2 = new Stylist($name2, $services2, $phone2, $id);
+            $stylist_test2->save();
+
+            Stylist::deleteAll();
+            $result = Stylist::getAll();
+
+            $this->assertEquals([], $result);
+        }
+
+        function test_find()
+        {
+            $name = "Gretchen";
+            $services = "Cut/Style, Color, Highlights";
+            $phone = "5033902341";
+            $id = null;
+            $stylist_test = new Stylist($name, $services, $phone, $id);
+            $stylist_test->save();
+
+            $name2 = "Mike";
+            $services2 = "Cut/Style, Men's Cut";
+            $phone2 = "971234903";
+            $stylist_test2 = new Stylist($name2, $services2, $phone2, $id);
+            $stylist_test2->save();
+
+            $result = Stylist::find($stylist_test2->getId());
+
+            $this->assertEquals($services2, $result->getServices());
+        }
+
+        function test_deleteStylist()
+        {
+            $name = "Gretchen";
+            $services = "Cut/Style, Color, Highlights";
+            $phone = "5033902341";
+            $id = null;
+            $stylist_test = new Stylist($name, $services, $phone, $id);
+            $stylist_test->save();
+
+            $name2 = "Mike";
+            $services2 = "Cut/Style, Men's Cut";
+            $phone2 = "971234903";
+            $stylist_test2 = new Stylist($name2, $services2, $phone2, $id);
+            $stylist_test2->save();
+
+            $stylist_test2->delete();
+            $result = Stylist::getAll();
+
+            $this->assertEquals([$stylist_test], $result);
+        }
+
+    }
 
 
 
