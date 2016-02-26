6761de3a26c54cea34d755e7eef04def3f41a9af
Aundra Miller <miller.aundra@gmail.com>
2016-02-26 10:52:10 -0800

    add route for adding new stylists

--- app/app.php
+++ app/app.php
@@ -23,6 +23,19 @@
         return $app['twig']->render('index.html.twig', array('stylists'=>$stylists));
     });
 
+    $app->post('/stylist/add', function() use ($app) {
+        $app['debug']= true;
+        $new_stylist = new Stylist($_POST['name'], $_POST['services'], $_POST['phone']);
+        $new_stylist->save();
+        $stylists = Stylist::getAll();
+        return $app['twig']->render('index.html.twig', array('stylists'=>$stylists));
+    });
+
+    $app->get('/stylist/{id}', function($id) use ($app) {
+        $stylist = Stylist::find($id);
+        
+    })
+
     return $app;
 
  ?>
